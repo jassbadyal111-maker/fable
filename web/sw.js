f0746a65b0cbe2f24329cdc2845a3bcf4db7dd7d
jassbadyal111-maker <jassbadyal111@gmail.com>
2026-09-07 07:07:14 +0530

    Cache dedicated syntax highlighter

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fable-shell-v3';
-const SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/capability-runtime.js', '/ui-fixes.js', '/manifest.webmanifest'];
+const CACHE = 'fable-shell-v4';
+const SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/syntax.js', '/capability-runtime.js', '/ui-fixes.js', '/manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
